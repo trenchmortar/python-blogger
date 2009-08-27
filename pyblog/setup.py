b232037b96a2b9ddfdffd9916e2e0d0fc9bcbe52
waltercruz <waltercruz@5388754e-bb53-0410-b6f5-594254728391>
2009-08-27 15:08:48 +0000

    Fixing setup.py

--- pyblog/setup.py
+++ pyblog/setup.py
@@ -6,11 +6,14 @@ version = '1.0'
 setup(name='pyblog',
       version=version,
       description="This library provides a pure python interface for the various blogging API.",
-      long_description="""\
-This library provides a pure python interface for the various blogging API.""",
-      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
-      keywords='',
-      author='Currently, only Metaweblog and Wordpress are implemented but it will be extended to use MovableType? and Blogger soon. Since Blogger has now moved onto GData API, this interface will internally use GData API rather then XML-RPC.Ritesh Nadhani',
+      long_description=open('README').read(),
+      classifiers=[
+      "Intended Audience :: Developers",
+      "Programming Language :: Python",
+      "Development Status :: 6 - Mature"
+      ],
+      keywords='blog api',
+      author='Ritesh Nadhani',
       author_email='riteshn@gmail.com',
       url='http://code.google.com/p/python-blogger/',
       license='BSD',
